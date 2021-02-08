c62fed2cd19efdfa3a3c1927b1cbd8047151d1c1
jessiek208 <73068180+jessiek208@users.noreply.github.com>
2021-02-07 20:42:26 -0500

    Make sass files

--- projects/evaluate-news-nlp/webpack.config.js
+++ projects/evaluate-news-nlp/webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
             test: '/\.js$/',
             exclude: /node_modules/,
             loader: "babel-loader"
-                }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
         ]
     },
     plugins: [
